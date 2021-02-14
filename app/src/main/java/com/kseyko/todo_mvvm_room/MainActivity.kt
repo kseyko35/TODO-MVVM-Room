b7af194b929ab51a1b15d3c0354d8814c08a01c5
kseyko35 <seyfiercan35@hotmail.com>
2021-02-14 22:17:10 +0300

    Project library structure created and it is first commit

---
+++ app/src/main/java/com/kseyko/todo_mvvm_room/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.kseyko.todo_mvvm_room
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
