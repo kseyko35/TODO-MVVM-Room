dbcb71b6154ac6d9a0e122b0f014be34a345014c
kseyko35 <seyfiercan35@hotmail.com>
2021-02-15 22:35:35 +0300

    Ui created

--- app/src/main/java/com/kseyko/todomvvmroom/ui/activity/MainActivity.kt
+++ app/src/main/java/com/kseyko/todomvvmroom/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.kseyko.todoMvvmRoom
+package com.kseyko.todomvvmroom.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kseyko.todomvvmroom.R
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
