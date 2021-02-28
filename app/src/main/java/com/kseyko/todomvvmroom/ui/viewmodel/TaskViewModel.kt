86c7693de88f8ef33cfbf44b22abbac50033a2a3
kseyko35 <seyfiercan35@hotmail.com>
2021-02-28 22:56:39 +0300

    Datastore is added instead of shared references

--- app/src/main/java/com/kseyko/todomvvmroom/ui/viewmodel/TaskViewModel.kt
+++ app/src/main/java/com/kseyko/todomvvmroom/ui/viewmodel/TaskViewModel.kt
@@ -2,11 +2,15 @@ package com.kseyko.todomvvmroom.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.kseyko.todomvvmroom.data.PreferencesManager
+import com.kseyko.todomvvmroom.data.SortOrder
 import com.kseyko.todomvvmroom.data.local.TaskDao
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
@@ -20,24 +24,39 @@ import javax.inject.Inject
 ╚════════════════════════════╝
  */
 @HiltViewModel
-class TaskViewModel @Inject constructor(private val taskDao: TaskDao) : ViewModel(){
+class TaskViewModel @Inject constructor(
+    private val taskDao: TaskDao,
+    private val preferencesManager: PreferencesManager
+) : ViewModel() {
 
     val searchQuery = MutableStateFlow("")
 
-    val sortOrder = MutableStateFlow( SortOrder.BY_DATE)
+    val preferencesFlow = preferencesManager.preferencesFlow
 
-    val hideCompleted = MutableStateFlow( false)
-
-    private val tasksFlow = combine(searchQuery,sortOrder,hideCompleted){
-        query,sortOrder,hideCompleted ->
-        Triple(query,sortOrder,hideCompleted)
-    }.flatMapLatest {//(query,sortOrder,hideCompleted)->
-        taskDao.getTasks(it.first,it.second,it.third)
+    /*val sortOrder = MutableStateFlow(SortOrder.BY_DATE)
+    val hideCompleted = MutableStateFlow(false)
+    private val tasksFlow =
+        combine(searchQuery, sortOrder, hideCompleted) { query, sortOrder, hideCompleted ->
+            Triple(query, sortOrder, hideCompleted)
+        }.flatMapLatest {//(query,sortOrder,hideCompleted)->
+            taskDao.getTasks(it.first, it.second, it.third)
+        }*/
+    private val tasksFlow = combine(searchQuery, preferencesFlow) { query, filterPreferences ->
+        Pair(query, filterPreferences)
+    }.flatMapLatest { (query, filterPreferences) ->
+        taskDao.getTasks(
+            query,
+            filterPreferences.sortOrder,
+            filterPreferences.hideCompleted
+        )
     }
-
-
+    fun onSortOrderSlected(sortOrder: SortOrder)= viewModelScope.launch {
+        preferencesManager.updateSortOrder(sortOrder)
+    }
+    fun onHideCompletedClick(hideCompleted:Boolean)= viewModelScope.launch {
+        preferencesManager.updateHideCompleted(hideCompleted)
+    }
 
 
     val tasks = tasksFlow.asLiveData()
 }
-enum class SortOrder{ BY_NAME, BY_DATE }
