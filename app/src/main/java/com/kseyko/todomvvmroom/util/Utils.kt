da51fa706b6635cfebcf5eb09696e69a29520388
kseyko35 <seyfiercan35@hotmail.com>
2021-03-20 00:57:55 +0300

    Some attributes are added

---
+++ app/src/main/java/com/kseyko/todomvvmroom/util/Utils.kt
@@ -0,0 +1,14 @@
+package com.kseyko.todomvvmroom.util
+
+
+/**     Code with ❤
+╔════════════════════════════╗
+║   Created by Seyfi ERCAN   ║
+╠════════════════════════════╣
+║  seyfiercan35@hotmail.com  ║
+╠════════════════════════════╣
+║      17,March,2021      ║
+╚════════════════════════════╝
+ */
+val <T> T.exhaustive: T
+    get() = this
